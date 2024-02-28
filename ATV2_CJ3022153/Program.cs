06057848d160fbff06296b3cad6aa518310368b3
Pedro Henrique <santos.pedro3@aluno.ifsp.edu.br>
2024-02-28 08:55:32 -0300

    Adicionar arquivos de projeto.

---
+++ ATV2_CJ3022153/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ATV2_CJ3022153
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //////////////////// EXERCICICO 2 //////////////////// 
+
+            float salario;
+
+            Console.WriteLine("Entre com o seu Salário:");
+            
+            salario = float.Parse( Console.ReadLine() );
+
+            salario = salario + (25.0f/100.0f) * salario;
+
+            Console.WriteLine("O novo salário é R$ {0}", salario);
+        } 
+    }
+}         
